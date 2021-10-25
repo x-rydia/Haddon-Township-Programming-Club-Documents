f3ee27d069b4cc1419fd7dd063286547c5dc6de9
x-rydia <70984848+x-rydia@users.noreply.github.com>
2021-10-25 10:51:13 -0400

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,13 @@
+#When we solve an equation 2x=10 in math class, we call x a variable with an unknown value. 
+#We willl use variables slightly differently in python, we will declare them with a value immediatley
+#such that:
+
+x = 5
+
+#Notice that that code did not have a '#' before it, having '#' before a piece of code denotes a comment.
+#A comment is code that is not treated as code by your computer. Feel free to write anything in a comment.
+#but keep it SFW, you dont want to hurt your computer's feelings. They are sensitive, lmao. 
+
+#We can reasign a value to x by redeclaring the variable
+
+x = 7
